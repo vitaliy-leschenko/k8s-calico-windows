cc9c8bc6db1bc51acab5db735fa823fae095db10
Vitaliy Leschenko <v.leschenko@digiteum.com>
2021-12-15 17:46:04 +0300

    initial commit

---
+++ hostprocess/build.ps1
@@ -0,0 +1,23 @@
+param(
+    [string]$repository = "vleschenko",
+    [version]$calicoVersion = "3.21.2"
+    [version]$minK8sVersion = "1.22.0"
+)
+
+pushd calico
+./build.sh -r $repository --calicoVersion "v$calicoVersion"
+
+$versions = (curl -L k8s.gcr.io/v2/kube-proxy/tags/list | ConvertFrom-Json).tags
+foreach($version in $versions)
+{
+    if ($version -match "^v(\d+\.\d+\.\d+)$")
+    {
+        $testVersion = [version]$Matches[1]
+        if ($testVersion -ge $minK8sVersion)
+        {
+            ./build.sh -r $repository --proxyVersion $version
+        }
+    }
+}
+
+popd
